c85320c2aa51d23da7b9289f4e91689444ce6df3
Muhammad Imran <44212121+muimran@users.noreply.github.com>
2026-03-25 15:23:14 +0600

    Export firebaseConfig and update email list

--- review_site/firebase-config.js
+++ review_site/firebase-config.js
@@ -1,16 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB-a5AiCvg7Gvj69JEVer38kaBjg7f4PLI",
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-a5AiCvg7*****JEVer38kaBjg7f4PLI",
   authDomain: "lunar-alpha-323506.firebaseapp.com",
   projectId: "lunar-alpha-323506",
   storageBucket: "lunar-alpha-323506.firebasestorage.app",
   messagingSenderId: "121998956871",
-  appId: "1:121998956871:web:a5ef3600c64016f9ea1462"
+  appId: "1:121998956871:web:a5ef3600c64016f9ea1462",
 };
 
 export const allowedEmails = ["your@gmail.com"];
